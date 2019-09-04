64e65b440fad1a1ad96a1371cf5c493f03e6b3dc
Petro Tsapei <petro@A1370s-MacBook-Air.local>
2019-09-04 22:17:39 +0300

    remove timezone

--- student-reminder-server/src/controllers/lesson.js
+++ student-reminder-server/src/controllers/lesson.js
@@ -159,7 +159,7 @@ exports.getById = async function (req, res) {
         },
         schedule: {
           value: result.schedule,
-          label: `${moment.weekdays(schedule.dayOfWeek)} (${moment(schedule.startTime).tz(+req.query.timezone).format('LT')} - ${moment(schedule.endTime).format('LT')}) - ${schedule.numberInSchedule}`
+          label: `${moment.weekdays(schedule.dayOfWeek)} (${moment(schedule.startTime).format('LT')} - ${moment(schedule.endTime).format('LT')}) - ${schedule.numberInSchedule}`
         },
         teacher: {
           value: result.teacher,
