58db5602dcca8254c93104f9b8f36ac66bbed2f6
Petro Tsapei <petro@A1370s-MacBook-Air.local>
2019-09-04 22:08:37 +0300

    ds

--- student-reminder-server/src/controllers/lesson.js
+++ student-reminder-server/src/controllers/lesson.js
@@ -146,7 +146,7 @@ exports.getById = async function (req, res) {
       const schedule = await ScheduleModel.findById(result.schedule);
       const teacher = await UserModel.findById(result.teacher);
 
-      console.log(typeof req.query.timezone);
+      console.log(req.query.timezone);
 
       res.json({
         subject: {
@@ -159,7 +159,7 @@ exports.getById = async function (req, res) {
         },
         schedule: {
           value: result.schedule,
-          label: `${moment.weekdays(schedule.dayOfWeek)} (${moment(schedule.startTime).utcOffset(+req.query.timezone).format('LT')} - ${moment(schedule.endTime).format('LT')}) - ${schedule.numberInSchedule}`
+          label: `${moment.weekdays(schedule.dayOfWeek)} (${moment(schedule.startTime).tz(+req.query.timezone).format('LT')} - ${moment(schedule.endTime).format('LT')}) - ${schedule.numberInSchedule}`
         },
         teacher: {
           value: result.teacher,
